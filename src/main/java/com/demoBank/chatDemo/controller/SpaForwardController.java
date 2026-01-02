e3be6f956d23a1a045a8743b465b897860df7c69
asherplotnik <aserplotnik@gmail.com>
2026-01-02 14:20:57 +0200

    added frontend

---
+++ src/main/java/com/demoBank/chatDemo/controller/SpaForwardController.java
@@ -0,0 +1,12 @@
+package com.demoBank.chatDemo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaForwardController {
+
+    @RequestMapping(value = {"/","/{path:^(?!api$).*$}","/**/{path:^(?!api$).*$}"})
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
